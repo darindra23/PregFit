b6c8acb69fcbaa3e9782cbbfd11cff947072d110
Dheo Gildas Varian <dheogildasvarian@gmail.com>
2021-06-21 12:40:05 +0700

    Add Notification

--- PregFit/Manager/Notification Manager/NotificationManager.swift
+++ PregFit/Manager/Notification Manager/NotificationManager.swift
@@ -9,6 +9,10 @@ import Foundation
 import UserNotifications
 
 class NotificationManager: NSObject, UNUserNotificationCenterDelegate{
+    static var shared = NotificationManager()
+    private override init() {}
+    
+    let notificationCenter = UNUserNotificationCenter.current()
     
     func userRequest() {
         let options: UNAuthorizationOptions = [.alert, .sound, .badge]
@@ -17,5 +21,30 @@ class NotificationManager: NSObject, UNUserNotificationCenterDelegate{
                 print("User has declined notifications")
             }
         }
+    }
+    
+    func scheduleNotificationForReminderTask() {
+        let content = UNMutableNotificationContent()
+        content.title = "WARNING!"
+        content.body = "Your Heart Rate exceeded 140 BPM. Consider toning down or taking a momentary break."
+        content.sound = UNNotificationSound.default
+        
+        //        let date = Date()
+        //        let triggerDate = Calendar.current.dateComponents([.day], from: date)
+        //        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0, repeats: false)
+        let identifier = "Reminder Task"
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        notificationCenter.add(request) { (error) in
+            if let error = error {
+                print("error \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.sound, .badge, .banner])
     }
 }
